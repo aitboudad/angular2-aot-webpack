b3550321f12b1f3742ee553e40b9dd3524438b8b
Abdellatif Ait boudad <a.aitboudad@gmail.com>
2017-01-15 21:16:22 +0000

    webpack-3972

--- src/app/modules/main.module.ts
+++ src/app/modules/main.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { UIRouterModule } from '../../../node_modules/ui-router-ng2/src/ng2';
 import { HelloWorldComponent } from '../components/hello-world.component';
 
 export { HelloWorldComponent };
@@ -7,7 +8,10 @@ export { HelloWorldComponent };
 @NgModule({
   bootstrap: [HelloWorldComponent],
   declarations: [HelloWorldComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    UIRouterModule.forRoot(),
+  ],
   providers: []
 })
 export class MainModule {}
